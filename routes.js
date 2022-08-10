7337db04393da3c36e49c8b162d46c397ba6b7b4
ChinhTQ <chinhtq@saisystem.vn>
2022-08-10 11:05:03 +0700

    update server

--- routes.js
+++ routes.js
@@ -10,6 +10,14 @@ var body = [];
 const handleRequestListener = (req, res) => {
     var url = req.url;
 
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     req.on('data', (chunk) => {
         body.push(chunk);
     });
